4f2bcf2ad1343636b2d7ef84ab588d5dbefb5f3d
HerTecSAdeCV <hertec2021@gmail.com>
2022-10-24 15:08:11 -0500

    Environment

--- StackStudent/src/environments/environment.prod.ts
+++ StackStudent/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'utnplataforma',
+    appId: '1:809274284014:web:ad72072ee29ac7ef72a0ff',
+    databaseURL: 'https://utnplataforma-default-rtdb.firebaseio.com',
+    storageBucket: 'utnplataforma.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD8wq_B105dW5hjm7cZciGl8pLGJe8mEXg',
+    authDomain: 'utnplataforma.firebaseapp.com',
+    messagingSenderId: '809274284014',
+  },
   production: true
 };
